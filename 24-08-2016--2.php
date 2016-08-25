7fa91a867b9ddceda95f673968fcab259741d626
ferrerguzman <ferrerguzmanmita@gmail.com>
2016-08-25 17:21:31 -0500

    agregando algunas cosas mas

---
+++ 24-08-2016--2.php
@@ -0,0 +1,6 @@
+<?php 
+	echo $_GET['textNombre']; 
+	echo "<br>";
+	echo $_GET['submitHola'];
+	
+?>
---
+++ 24-08-2016--4.php
@@ -0,0 +1,8 @@
+<?php 
+	echo $_POST['textNombre']; 
+	echo "<br>";
+	echo $_POST['submitHola'];
+	var_dump($_POST);
+	echo "<br>";
+	echo "<pre>";print_r($_POST);echo("</pre");
+?>
